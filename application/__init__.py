f3e1823cbe6d81600e6003bc83c56ff1e829b01e
Heikki Pulli <heikki.pulli@helsinki.fi>
2020-01-18 17:24:57 +0200

    psql configurations for heroku

--- application/__init__.py
+++ application/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
-app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
