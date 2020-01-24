2947f00914d0c5e7e95e50bb4934a738d778ae2a
Heikki Pulli <heikki.pulli@helsinki.fi>
2020-01-24 18:06:43 +0200

    password from 60 to 120

--- application/user/Models.py
+++ application/user/Models.py
@@ -5,8 +5,8 @@ class User(db.Model):
     __tablename__ = "accounts"
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(60), nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    username = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
